7e266dab45b825e05d3ceb438b2ccbc5733f2c66
kyuung <mel1521@naver.com>
2021-10-26 16:43:19 +0900

    Add elements

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import Modal from '../components/Modal'
+import { FlexRow, FlexColumn, SubTitle, Title, Input } from '../elements/index'
 
 const Main = () => {
-    return (
-        <div>
-            <Modal />
-        </div>
-    )
+    return <></>
 }
 
 export default Main
