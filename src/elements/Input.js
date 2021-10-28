dfda210d74f63e5e1b559a06f5955bd4dd84a71c
sunysty <seokjiseon@gmail.com>
2021-10-28 16:20:55 +0900

    Edit input타입추가, subtitle fontsize 1.5rem으로 수정

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -17,6 +17,7 @@ Input.defaultProps = {
     _onChange: () => {},
     _ph: '내용을 입력하세요.',
     _others: false,
+    _type: ''
 }
 
 const InputEl = styled.input`
@@ -26,9 +27,9 @@ const InputEl = styled.input`
     padding-left: 0.8rem;
     width: 20rem;
     height: 3rem;
-    border: 1px solid li
-    ghtgray;
+    border: 1px solid lightgray;
     ${(props) => (props._others ? props._others : '')};
+    ${(props) => (props._tye ? props._type : '')};
 `
 
 export default Input
--- src/elements/SubTitle.js
+++ src/elements/SubTitle.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SubTitle = (props) => {
-    const { _align, children, _others } = props
-    const styles = { _align, _others }
+    const { _align, children, _others, _margin } = props
+    const styles = { _align, _others, _margin }
     return (
         <>
             <SubTitleEl {...styles}>{children}</SubTitleEl>
@@ -15,14 +15,16 @@ SubTitle.defaultProps = {
     childern: '',
     _align: 'center',
     _others: false,
+    _margin: '1rem'
 }
 
 const SubTitleEl = styled.h1`
     text-align: ${(props) => props._align};
-    font-size: 2rem;
+    font-size: 1.5rem;
     font-weight: 600;
     color: 'black';
     ${(props) => (props._others ? props._others : '')};
+    margin: ${(props) => props._margin};
 `
 
 export default SubTitle
