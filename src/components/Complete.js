860d4ab02804a43905c1b30f9c6d1192b9e76607
jinsun <yks9013@naver.com>
2021-11-02 20:49:07 +0900

    Edit Complete, Slice

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -15,9 +15,10 @@ import {
     setActionName,
     setModal,
     setImgSrc,
-    setcompleteBtn,
-    setdefaultBtn,
+    setCompleteBtn,
+    setDefaultBtn,
 } from '../redux/modules/completeSlice'
+import { CompleteAfter } from '../components/index'
 
 const CompleteActionModal = (props) => {
     const dispatch = useDispatch()
@@ -26,8 +27,20 @@ const CompleteActionModal = (props) => {
     const modalImg = useSelector((state) => state.setModal.ImgSrc)
     const modalActionName = useSelector((state) => state.setModal.actionName)
     const modalActionBtn = useSelector((state) => state.setModal.actionBtn)
-    const modalcompleteBtn = useSelector((state) => state.setModal.completeBtn)
-    const modaldefaultBtn = useSelector((state) => state.setModal.defaultBtn)
+    const modalCompleteBtn = useSelector((state) => state.setModal.completeBtn)
+    const modalDefaultBtn = useSelector((state) => state.setModal.defaultBtn)
+
+    const successAction = () => {
+        dispatch(setDefaultBtn(false))
+        dispatch(setCompleteBtn(true))
+        dispatch(setActionName('성공!'))
+        dispatch(
+            setImgSrc(
+                'https://s3.ap-northeast-2.amazonaws.com/sunnieee.shop/mingsuccess.jpg'
+            )
+        )
+        dispatch(setActionBtn('닫기'))
+    }
 
     const changeBtn = () => {
         dispatch(
@@ -38,14 +51,32 @@ const CompleteActionModal = (props) => {
         if (modalActionBtn == '시작 !') {
             dispatch(setActionBtn('화이팅 !'))
             setTimeout(function () {
-                dispatch(setdefaultBtn(false))
-                dispatch(setcompleteBtn(true))
+                dispatch(setActionBtn('완료하기 !'))
+                dispatch(setDefaultBtn(false))
+                dispatch(setCompleteBtn(true))
             }, 5000)
         }
     }
 
-    {
-        success && (
+    const changeFalse = () => {
+        dispatch(setModal(false))
+        dispatch(
+            setImgSrc(
+                'https://s3.ap-northeast-2.amazonaws.com/sunnieee.shop/ming.JPG'
+            )
+        )
+        dispatch(setActionBtn('시작 !'))
+        dispatch(setDefaultBtn(true))
+        dispatch(setCompleteBtn(false))
+    }
+
+    return (
+        <>
+            {success && (
+                <div style={{ display: 'flex', zIndex: '30' }}>
+                    <CompleteAfter />
+                </div>
+            )}
             <FlexRow _border={'none'}>
                 <div
                     style={{
@@ -58,7 +89,7 @@ const CompleteActionModal = (props) => {
                         left: '0',
                     }}
                     onClick={() => {
-                        setSuccess(false)
+                        changeFalse()
                     }}
                 ></div>
                 <ModalEl>
@@ -71,13 +102,13 @@ const CompleteActionModal = (props) => {
                             cursor: 'pointer',
                         }}
                         onClick={() => {
-                            setSuccess(false)
+                            changeFalse()
                         }}
                     >
                         <CancelRounded />
                     </button>
                     <Text _fontSize={'2rem'} _margin={'1.5rem 0px 0.5rem 0px'}>
-                        성공
+                        {modalActionName}
                     </Text>
                     <Img
                         _src={modalImg}
@@ -86,88 +117,38 @@ const CompleteActionModal = (props) => {
                         _bradius={'0px'}
                         _others={'background-color:#fff'}
                     ></Img>
-                    <ButtonOutlined
-                        _width={'13rem'}
-                        _margin={'1rem 0px 0px 0px'}
-                        _padding={'0px'}
-                        _onClick={() => {
-                            setSuccess(false)
-                        }}
-                    >
-                        <Text _padding={'0.7rem 0px'}>닫기</Text>
-                    </ButtonOutlined>
+                    {modalDefaultBtn && (
+                        <ButtonOutlined
+                            _width={'13rem'}
+                            _margin={'1rem 0px 0px 0px'}
+                            _padding={'0px'}
+                            _onClick={() => {
+                                changeBtn()
+                            }}
+                        >
+                            <Text _padding={'0.7rem 0px'}>
+                                {modalActionBtn}
+                            </Text>
+                        </ButtonOutlined>
+                    )}
+                    {modalCompleteBtn && (
+                        <ButtonOutlined
+                            _width={'13rem'}
+                            _margin={'1rem 0px 0px 0px'}
+                            _padding={'0px'}
+                            _onClick={() => {
+                                successAction()
+                                modalActionBtn == '닫기' && changeFalse()
+                            }}
+                        >
+                            <Text _padding={'0.7rem 0px'}>
+                                {modalActionBtn}
+                            </Text>
+                        </ButtonOutlined>
+                    )}
                 </ModalEl>
             </FlexRow>
-        )
-    }
-
-    return (
-        <FlexRow _border={'none'}>
-            <div
-                style={{
-                    width: '100vw',
-                    height: '100vh',
-                    backgroundColor: 'black',
-                    opacity: '0.3',
-                    position: 'fixed',
-                    top: '0',
-                    left: '0',
-                }}
-                onClick={() => {
-                    dispatch(setModal(false))
-                }}
-            ></div>
-            <ModalEl>
-                <button
-                    style={{
-                        position: 'absolute',
-                        right: '1rem',
-                        border: 'none',
-                        background: 'none',
-                        cursor: 'pointer',
-                    }}
-                    onClick={() => {
-                        dispatch(setModal(false))
-                    }}
-                >
-                    <CancelRounded />
-                </button>
-                <Text _fontSize={'2rem'} _margin={'1.5rem 0px 0.5rem 0px'}>
-                    {modalActionName}
-                </Text>
-                <Img
-                    _src={modalImg}
-                    _width={'13rem'}
-                    _height={'18rem'}
-                    _bradius={'0px'}
-                    _others={'background-color:#fff'}
-                ></Img>
-                {modaldefaultBtn && (
-                    <ButtonOutlined
-                        _width={'13rem'}
-                        _margin={'1rem 0px 0px 0px'}
-                        _padding={'0px'}
-                        _onClick={() => {
-                            changeBtn()
-                        }}
-                    >
-                        <Text _padding={'0.7rem 0px'}>{modalActionBtn}</Text>
-                    </ButtonOutlined>
-                )}
-                {modalcompleteBtn && (
-                    <ButtonOutlined
-                        _width={'13rem'}
-                        _margin={'1rem 0px 0px 0px'}
-                        _padding={'0px'}
-                        _onClick={() => {
-                            console.log('완료버튼 누름름')
-                        }}
-                    >
-                        <Text _padding={'0.7rem 0px'}>완료하기 !</Text>
-                    </ButtonOutlined>
-                )}
-            </ModalEl>
-        </FlexRow>
+        </>
     )
 }
 
