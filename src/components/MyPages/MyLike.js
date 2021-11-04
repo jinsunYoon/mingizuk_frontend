238da33a3e9d2a72f1ef1fa21b010526f359da11
sunysty <seokjiseon@gmail.com>
2021-11-04 10:34:26 +0900

    Add MyLike components

--- src/components/MyPages/MyLike.js
+++ src/components/MyPages/MyLike.js
@@ -1,25 +1,75 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import Icon from '../../components/icons/Icon'
 import styled from 'styled-components';
-import MyMoimPost from '../MyPages/MyMoimPost'
+
+import { Text } from '../../elements/index'
+import { NavBar } from '../../components/index'
+import { myMoimLikeMD } from '../../redux/async/myMoim';
 
 
 const MyLike = () => {
-    
-    const data = {
-        title: '주 2회 공원 걷기 모임',
-        writer: 'kyuung',
-        people: 8,
-        publisedDate: '2021.10.11',
-        likeCount: 5,
-        commentCount: 3,
-        contents:'서울 한강에서 매주 수,금 걷기운동 하실분 구합니다!'
-    }
+    const dispatch = useDispatch()
+    const like_list = useSelector((state)=>state.myMoim.my_like)
+    console.log(like_list)
 
+   React.useEffect(()=>{
+        dispatch(myMoimLikeMD())
+    },[])
+
+    
+    // 모임좋아요기능 끝나면, 마이모임좋아요기능 추가하기!
     return(
-        <>
-            <MyMoimPost/>
+         <>
+         
+        {/* <PostBox>
+            <TitleBox>
+                <div>
+                    <Text 
+                        _fontSize="16px"
+                    >
+                        {data.title}
+                    </Text>
+                    <Text 
+                        _fontSize="11px" 
+                        _color="#8f8f8f"
+                        _align="left"
+                        >
+                        {data.publisedDate} {data.writer}
+                    </Text>
+                </div>
+                <Text _fontSize="11px">참여자 : {data.people}</Text>
+            </TitleBox>
+            <EtcBox>
+                <SmallBox>
+                    <Icon icon={'favorite'} size="20px" /> 좋아요
+                    {data.likeCount}개
+                </SmallBox>
+                <SmallBox>
+                    <Icon icon={'message'} size="20px" />
+                    댓글 {data.commentCount}개
+                </SmallBox>
+            </EtcBox>
+        </PostBox> */}
+        <NavBar/>
         </>
     )
+
 }
+
+const TitleBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 15px;
+`
+
+const PostBox = styled.div`
+    margin: 5vw auto;
+    width: 90vw;
+    height: auto;
+    border: 1px solid #c4c4c4;
+    position: relative;
+`
+
 
 export default MyLike;
