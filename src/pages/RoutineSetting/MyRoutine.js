8a838589b7ebe99e4abd74f405babae92a5b184c
kyuung <mel1521@naver.com>
2021-11-05 23:27:06 +0900

    📦  Update Routine Style to SCSS

--- src/pages/RoutineSetting/MyRoutine.js
+++ src/pages/RoutineSetting/MyRoutine.js
@@ -1,34 +1,31 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import RoutineTemplate from '../../components/Routine/RoutineTemplate'
-import ToggleTab from '../../components/ToggleTab'
+import { Header } from '../../components'
 import RoutineDesc from '../../components/Routine/RoutineDesc'
-import { Header, NavBar } from '../../components'
+import ToggleTab from '../../components/ToggleTab'
+import { useSelector } from 'react-redux'
 import { history } from '../../redux/store'
+import '../../styles/routine/my-routine.scss'
 
 const MyRoutine = () => {
     const status = useSelector((state) => state.routine.myPage)
-    console.log(status)
 
     return (
         <>
-            <RoutineTemplate
-                button="메인 루틴으로 설정하기"
-                _onClick_={() => history.push('/')}
-                is_button="false"
-                addBtn="true"
-            >
-                <div style={{ zIndex: '3' }}>
-                    <Header name="루틴 설정하기" />
-                </div>
+            <Header />
+            <section className="container">
                 <ToggleTab
                     firstValue={'내 루틴'}
                     secondValue={'추천 루틴'}
                     select={status}
                 />
                 <RoutineDesc select={status} />
-                <NavBar />
-            </RoutineTemplate>
+                <button
+                    className="add-btn"
+                    onClick={() => history.push('/routine/add')}
+                >
+                    +
+                </button>
+            </section>
         </>
     )
 }
