9e82beac4c63c46099fa8cbb6b804393f084a310
sunysty <seokjiseon@gmail.com>
2021-11-10 16:51:52 +0900

    ✅ Update moimpage & collection

--- src/components/MyPages/CollectionItem.js
+++ src/components/MyPages/CollectionItem.js
@@ -1,51 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 //*style
 import styled from 'styled-components'
-import LockIcon from '@mui/icons-material/Lock';
+import LockIcon from '@mui/icons-material/Lock'
 
+// 레벨별(1.Lv ~10.Lv)
 const CollectionItem = () => {
-    const [collection, setCollection] = useState(false);
-    
-    return(
+    const [collection, setCollection] = useState(false)
+
+    return (
         <>
-            {
-                collection    
-            ?
+            {collection ? (
                 <ItemContainer>
-                    <Item/>        
-                </ItemContainer>    
-            :
-                <ItemContainer>
-                    <LockIcon/>
+                    <Item />
                 </ItemContainer>
-            }            
+            ) : (
+                <ItemContainer>
+                    <LockIcon />
+                </ItemContainer>
+            )}
         </>
     )
-        }
+}
 
 const Item = styled.img`
-    border:1px solid #000;
-    width:5rem;
-    height:5rem;
-    border-radius:50%;
-    display:flex;
-    position:absolute;
+    border: 1px solid #000;
+    width: 5rem;
+    height: 5rem;
+    border-radius: 50%;
+    display: flex;
+    position: absolute;
 `
 
 const ItemContainer = styled.div`
-    display:flex;
+    display: flex;
     flex-wrap: wrap;
-    justify-content:space-evenly;
-    align-content:space-around;
+    justify-content: space-evenly;
+    align-content: space-around;
     flex-direction: row;
-    border:1px solid #000;
-    width:5rem;
-    height:5rem;
-    border-radius:50%;
+    border: none;
+    width: 76px;
+    height: 76px;
+    border-radius: 50%;
+    background-color: #efefef;
 `
 
-
-
-export default CollectionItem;
-
+export default CollectionItem
