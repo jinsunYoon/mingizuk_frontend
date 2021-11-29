d8b8a89630f742c1bfd11e620048f2ed7e4ffc17
kyuung <mel1521@naver.com>
2021-11-30 01:45:09 +0900

    ✅ Update download, routine page

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -20,8 +20,15 @@ const Downlaod = () => {
         width: '6.7rem',
         height: 'auto',
     }
+
+    const installEvent = () => {
+        if (supported() && !isInstalled()) {
+            handleClick()
+        } else return
+    }
+
     return (
-        <div className="onBoarding-warp">
+        <div className="pleaz">
             <section className="onBoarding-container">
                 <article className="rion-main">
                     <img
@@ -36,31 +43,30 @@ const Downlaod = () => {
                         Mingizuk
                     </h1>
                     <div>
-                        {supported() && !isInstalled() && (
+                        <div className="install-btn-warp">
                             <button
                                 className="install-btn"
                                 type="button"
-                                onClick={handleClick}
+                                onClick={() => installEvent()}
                             >
-                                다운받기 ( 앱으로만 이용 하실 수 있어요. )
+                                다운받기 - 앱으로만 이용 하실 수 있어요 : )
                             </button>
-                        )}
-                        {isInstalled() && supported() && (
-                            <button className="install-btn" type="button">
-                                이미 기기에 밍기적이 있어요.
-                            </button>
-                        )}
-                        {!supported() && (
-                            <div>
-                                <button className="install-btn" type="button">
-                                    아이폰은 Safari , 안드로이드 / 데스크탑은 :
-                                    chorme을 이용해주세요. (시크릿탭 제외)
-                                </button>
-                                <p className="install-desc">
-                                    앱이 이미 깔려있다면 지우고 다시 깔아주세요.
-                                </p>
-                            </div>
-                        )}
+                            <h2>버튼이 안눌린다면?</h2>
+                            <p className="install-desc">
+                                1. 아이폰은 Safari , <br /> 안드로이드 /
+                                데스크탑은 : chorme을 이용해주세요. <br />
+                                (시크릿탭 제외)
+                            </p>
+                            <p className="install-desc">
+                                2. 앱이 이미 깔려있다면 지우고 다시 깔아주세요.
+                            </p>
+                            <p className="install-desc">
+                                3. 10초정도 기다렸다가 다시 눌러주세요.
+                            </p>
+                            <p className="install-desc">
+                                * 오류 문의 : Mingizuk@gmail.com
+                            </p>
+                        </div>
                     </div>
                 </article>
             </section>
